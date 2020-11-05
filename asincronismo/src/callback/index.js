91d3d47edeeb364153e4c6a9f2e5c2f783896505
Carlos Cuellar <carlos12cuellar@outlook.com>
2020-11-05 15:14:32 -0600

    ejemplos de callback

--- asincronismo/src/callback/index.js
+++ asincronismo/src/callback/index.js
@@ -9,7 +9,7 @@ function calc(num1, num2, callback) { //le pasamos un funcion que va ser el call
 
 }
 
-console.log(calc(2, 2, sum)); //invocamos la funcion y le pasamos los dos numeros y estos dos numeros se los pasamos como parametros a la funcion de callback para que retorne en ese caso la suma que es la funcion que se pasa como parametro en otra funcion
+console.log(calc(6, 2, sum)); //invocamos la funcion y le pasamos los dos numeros y estos dos numeros se los pasamos como parametros a la funcion de callback para que retorne en ese caso la suma que es la funcion que se pasa como parametro en otra funcion
 
 
 /**
@@ -21,3 +21,17 @@ console.log(calc(2, 2, sum)); //invocamos la funcion y le pasamos los dos numero
  * "callback": "node src/callback/index.js" y ya podremos desde la consola usar 
  * npm run callback y podremos ejecutar el archivo
  */
+
+function date(callback) { //funcion que imprimira la fecha y recibe como parametro una funcion callback(se usa este nombre por convenio no es obligacion)
+    console.log(new Date); //imprime una nueva fecha
+    setTimeout(() => { //setTimeOut recibe dos parametros una funcion y el tiempo de espera en este caso se le paso una arrow function
+        let date = new Date; //se declara una variable que guardara la fecha
+        callback(date); //se invoca la funcion callback y se le pasa la fecha
+    }, 3000); //se esperara 3s para imprimir otra fecha pero se imprimira desde la funcion callback
+}
+
+function printDate(date) { //funcion que recibe como parametro una fecha
+    console.log(date); //imprime una fecha
+}
+
+date(printDate); //invocamoa la funcion que recibe como parametro a un callback
